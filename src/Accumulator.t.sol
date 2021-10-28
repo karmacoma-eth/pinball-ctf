e8876f7a42ef50b20cbe7c03fe385260bac9a286
karmacoma <karmacoma.eth@gmail.com>
2021-10-28 09:54:54 -0700

    Add constraints for more ball packing fun

--- src/Accumulator.t.sol
+++ src/Accumulator.t.sol
@@ -4,19 +4,26 @@ pragma solidity ^0.8.9;
 import "ds-test/test.sol";
 
 contract AccumulatorTest is DSTest {
-    function youShallNotPass(uint16[10] memory state) pure public {
+    function youShallNotPass(uint16[10] memory inputs) pure public {
         uint32 accumulator = 1;
 
         unchecked {
             for (uint i = 0; i < 10; i++) {
-                accumulator *= uint32(state[i]);
+                accumulator *= uint32(inputs[i]);
             }
         }
 
         require(accumulator == 0x020c020c, "incorrect accumulator");
     }
 
-    function proveFail_weShallActuallyPass(uint16[10] memory state) public {
-        youShallNotPass(state);
+    function proveFail_weShallActuallyPass(uint16[10] memory inputs) public {
+        youShallNotPass(inputs);
+    }
+
+    function proveFail_weShallActuallyPassWithConstraints(uint16[10] memory inputs) public {
+        // require(inputs[0] == 0x0104); // <--- can't get this constraint to be satisfied
+        require(inputs[1] == 1);
+        require(inputs[2] & 0xff00 == 0xff00);
+        youShallNotPass(inputs);
     }
 }
